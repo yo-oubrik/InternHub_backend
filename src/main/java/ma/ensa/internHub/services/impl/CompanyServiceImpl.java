6e3d235af821027947128c5e1a865a4b9032b8bc
Youssef Oubrik <yo.oubrik@gmail.com>
2025-04-04 17:32:45 +0000

    feat: send welcome email after company registration

--- src/main/java/ma/ensa/internHub/services/impl/CompanyServiceImpl.java
+++ src/main/java/ma/ensa/internHub/services/impl/CompanyServiceImpl.java
@@ -1,5 +1,14 @@
 package ma.ensa.internHub.services.impl;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import lombok.RequiredArgsConstructor;
 import ma.ensa.internHub.domain.dto.request.CompanyRequest;
 import ma.ensa.internHub.domain.dto.response.CompanyResponse;
@@ -8,15 +17,7 @@ import ma.ensa.internHub.exception.DuplicateResourceException;
 import ma.ensa.internHub.mappers.CompanyMapper;
 import ma.ensa.internHub.repositories.CompanyRepository;
 import ma.ensa.internHub.services.CompanyService;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import ma.ensa.internHub.services.EmailNotificationService;
 
 @Service
 @RequiredArgsConstructor
@@ -24,17 +25,20 @@ public class CompanyServiceImpl implements CompanyService {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
     private final PasswordEncoder passwordEncoder;
+    private final EmailNotificationService emailNotificationService;
 
     @Override
     public CompanyResponse createCompany(CompanyRequest request) {
-        if (companyRepository.existsByEmail(request.getEmail())) {
+        String companyMail = request.getEmail();
+        if (companyRepository.existsByEmail(companyMail)) {
             throw new DuplicateResourceException("Email already exists");
         }
-
+        emailNotificationService.sendHtmlEmail(companyMail, "Welcome To InternHub", "welcome-company",
+                Map.of("companyName", request.getName()), Map.of("logo.png", "/static/logo.png"));
         Company company = companyMapper.toEntity(request);
         company.setPassword(passwordEncoder.encode(request.getPassword()));
-
         companyRepository.save(company);
+
         return companyMapper.toResponse(company);
     }
 
