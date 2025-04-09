c5a1b9b3c08671629d7ce1a8f647b2537eea54fe
Youssef Oubrik <yo.oubrik@gmail.com>
2025-04-09 16:34:12 +0000

    feat: handle unsupported HTTP methods with custom exception handler

--- src/main/java/ma/ensa/internHub/advice/GlobalExceptionHandler.java
+++ src/main/java/ma/ensa/internHub/advice/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -82,6 +83,14 @@ public class GlobalExceptionHandler {
                 return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
         }
 
+        @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+        public ResponseEntity<ApiErrorResponse> handleHttpRequestMethodNotSupported(
+                        HttpRequestMethodNotSupportedException ex,
+                        WebRequest request) {
+                ApiErrorResponse errorResponse = buildApiErrorResponse(HttpStatus.METHOD_NOT_ALLOWED,
+                                "Method not allowed", request);
+                return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
+        }
         @ExceptionHandler(Exception.class)
         public ResponseEntity<ApiErrorResponse> handleGlobalException(
                         Exception ex,
