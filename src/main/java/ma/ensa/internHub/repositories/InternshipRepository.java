c0478221c57364305ca0e1fe581aef7d91481bbe
BenJbaliMehdi <mehdi7jbali@gmail.com>
2025-04-12 01:59:40 +0100

    feat(Internship): update the InternshipRepository

--- src/main/java/ma/ensa/internHub/repositories/InternshipRepository.java
+++ src/main/java/ma/ensa/internHub/repositories/InternshipRepository.java
@@ -4,9 +4,12 @@ import ma.ensa.internHub.domain.entities.Internship;
 import ma.ensa.internHub.domain.entities.WorkMode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InternshipRepository extends JpaRepository<Internship, UUID> {
     long countByWorkMode(WorkMode workMode);
+
+    List<Internship> findByCompanyId(UUID companyId);
 
 }
