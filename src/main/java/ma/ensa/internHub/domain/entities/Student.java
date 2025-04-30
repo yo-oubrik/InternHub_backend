2c4f68c01207e191f1614ce9ecd98713d092cf96
Youssef Oubrik <yo.oubrik@gmail.com>
2025-04-30 22:49:55 +0000

    feat: add controller to handle student flags

--- src/main/java/ma/ensa/internHub/domain/entities/Student.java
+++ src/main/java/ma/ensa/internHub/domain/entities/Student.java
@@ -2,6 +2,7 @@ package ma.ensa.internHub.domain.entities;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.DiscriminatorValue;
@@ -50,21 +51,20 @@ public class Student extends User {
 
     private String profileDescription;
 
-     @OneToMany(mappedBy = "student" , cascade = CascadeType.ALL, orphanRemoval =
-     true)
-     private List<Experience> experiences;
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Experience> experiences;
 
-     @OneToMany(mappedBy = "student" , cascade = CascadeType.ALL, orphanRemoval =
-     true)
-     private List<Formation> formations;
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Formation> formations;
 
-     @OneToMany(mappedBy = "student" , cascade = CascadeType.ALL, orphanRemoval =
-     true)
-     private List<Project> projects;
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Project> projects;
 
-     @OneToMany(mappedBy = "student" , cascade = CascadeType.ALL, orphanRemoval =
-     true)
-     private List<Certificat> certificates;
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Certificat> certificates;
+    private boolean blocked;
+
+    private LocalDateTime blockedAt;
 
     @OneToMany(mappedBy = "flaggedByStudent", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
@@ -77,7 +77,6 @@ public class Student extends User {
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<Application> applications = new ArrayList<>();
-
 
     @Override
     public Role getRole() {
--- src/main/java/ma/ensa/internHub/services/StudentService.java
+++ src/main/java/ma/ensa/internHub/services/StudentService.java
@@ -1,12 +1,13 @@
 package ma.ensa.internHub.services;
 
-import ma.ensa.internHub.domain.dto.request.EmailVerificationRequest;
-import ma.ensa.internHub.domain.dto.request.StudentRequest;
-import ma.ensa.internHub.domain.dto.response.StudentResponse;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import ma.ensa.internHub.domain.dto.request.EmailVerificationRequest;
+import ma.ensa.internHub.domain.dto.request.NotificationRequest;
+import ma.ensa.internHub.domain.dto.request.StudentRequest;
+import ma.ensa.internHub.domain.dto.response.StudentResponse;
 
 public interface StudentService {
     StudentResponse createStudent(StudentRequest request);
@@ -14,12 +15,18 @@ public interface StudentService {
     long countStudents();
 
     List<StudentResponse> getAllStudents();
+
     StudentResponse getStudentById(UUID id);
+
     StudentResponse getStudentByEmail(String email);
+
     StudentResponse updateStudentById(UUID id, StudentRequest request);
 
     Map<String, Long> countStudentsByMonth();
 
     StudentResponse confirmAndRegisterStudent(EmailVerificationRequest request);
 
+    void blockStudent(UUID id, NotificationRequest request);
+
+    void unblockStudent(UUID id, NotificationRequest request);
 }
