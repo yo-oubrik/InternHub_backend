2f4a9241b1b426368afb019c8eda4cf80d4e5d74
BenJbaliMehdi <mehdi7jbali@gmail.com>
2025-05-01 01:20:16 +0100

    refactor(core): update internship repository, password reset, and user details logic

--- src/main/java/ma/ensa/internHub/repositories/PasswordResetTokenRepository.java
+++ src/main/java/ma/ensa/internHub/repositories/PasswordResetTokenRepository.java
@@ -2,10 +2,12 @@ package ma.ensa.internHub.repositories;
 
 import ma.ensa.internHub.domain.entities.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
 
--- src/main/java/ma/ensa/internHub/services/InternshipService.java
+++ src/main/java/ma/ensa/internHub/services/InternshipService.java
@@ -2,7 +2,6 @@ package ma.ensa.internHub.services;
 
 import ma.ensa.internHub.domain.dto.request.InternshipRequest;
 import ma.ensa.internHub.domain.dto.response.InternshipResponse;
-import ma.ensa.internHub.domain.entities.Internship;
 import ma.ensa.internHub.domain.entities.WorkMode;
 
 import java.util.List;
